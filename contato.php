457f4d9b2eaa23449ba81561b5dbcad30b602df2
Daniel Coder <netofire@netofire.com>
2015-05-05 21:35:50 -0300

    Landpage primeira versão. Form contato funcional

---
+++ contato.php
@@ -0,0 +1,43 @@
+<?php
+
+if (!isset($_POST) || !count($_POST))
+	exit;
+
+$dados = $_POST;
+$retorno = array();
+
+foreach ($_POST as $k => $v) {
+	$dados[$k] = trim(strip_tags($v));
+}
+
+if (!isset($dados['name']) || !isset($dados['email']) || !isset($dados['message'])) {
+	exit;
+} 
+
+if (empty($dados['name']) || empty($dados['email']) || empty($dados['message'])) {
+	$retorno['status'] = false;
+} else {
+	$destino = 'netofire@netofire.com';
+	$assunto = 'Mensagem enviada do site';
+	$headers = 'From: ' . strtolower($dados['email']) . "\r\n" .'
+MIME-Version: 1.0' . "\r\n" . '
+Content-type: text/html; charset=utf-8' . "\r\n";
+	$mensagem = '<html>
+<head>
+ <title>Mensagem enviada através do site</title>
+</head>
+<body><p>Mensagem enviada através do site:<br><br>
+Nome: <b>' . $dados['name'] . '</b><br>
+E-mail: <b>' . strtolower($dados['email']) . '</b><br>
+Mensagem: <b>' . $dados['message'] . '</b><br><br>
+IP: '. $_SERVER['REMOTE_ADDR'] . '<br>
+Data: ' . date("d/m/Y H:i") . '</p></body></html>';
+	
+	if (mail($destino, $assunto, $mensagem, $headers, '-fnetofire@netofire.com')) {
+		$retorno['status'] = true;
+	} else {
+		$retorno['status'] = false;
+	}
+	
+	print json_encode($retorno);
+}
--- index.php
+++ index.php
@@ -1,1 +1,167 @@
-<?php
+<!DOCTYPE HTML>
+<html>
+	<head>
+		<title>Daniel Coder :: Free Lancer Web Developer</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<!--[if lte IE 8]><script src="css/ie/html5shiv.js"></script><![endif]-->
+		<script src="js/jquery.min.js"></script>
+		<script src="js/jquery.poptrox.min.js"></script>
+		<script src="js/jquery.scrolly.min.js"></script>
+		<script src="js/jquery.scrollex.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel.css" />
+			<link rel="stylesheet" href="css/style.css" />
+			<link rel="stylesheet" href="css/style-wide.css" />
+			<link rel="stylesheet" href="css/style-normal.css" />
+		</noscript>
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+	</head>
+	<body>
+
+		<!-- Header -->
+			<header id="header">
+
+				<!-- Logo -->
+					<h1 id="logo">Daniel Coder</h1>
+
+				<!-- Nav -->
+					<nav id="nav">
+						<ul>
+							<li><a href="#intro">Intro</a></li>
+							<li><a href="#one">O que eu faço</a></li>
+							<li><a href="#two">Quem eu sou</a></li>
+							<li><a href="#work">Meu trabalho</a></li>
+							<li><a href="#contact">Contato</a></li>
+						</ul>
+					</nav>
+
+			</header>
+
+		<!-- Intro -->
+			<section id="intro" class="main style1 dark fullscreen">
+				<div class="content container 75%">
+					<header>
+						<h2>Daniel Coder</h2>
+					</header>
+					<p>Desenvolvedor Web <strong>free lancer</strong> especialista em 
+					análise, criação, aprimoramento e manutenção de <strong>websites, 
+					lojas virtuais e sistemas</strong> em PHP.</p>
+					<footer>
+						<a href="#one" class="button style2 down">Mais</a>
+					</footer>
+				</div>
+			</section>
+
+		<!-- One -->
+			<section id="one" class="main style2 right dark fullscreen">
+				<div class="content box style2">
+					<header>
+						<h2>O que eu faço</h2>
+					</header>
+					<p><strong>Analiso e resolvo</strong>. Simples assim.<br> 
+					Meu contratante apresenta a necessidade.
+					Eu analiso e indico qual a solução ideal para atender a todos os objetivos pelo <strong>melhor 
+					investimento</strong> possível, com
+					foco em <strong>padrões</strong> e <strong>desempenho</strong> para Internet.<br>
+					O resultado é um produto final de <strong>excelente qualidade</strong>.
+					</p>
+				</div>
+				<a href="#two" class="button style2 down anchored">Próximo</a>
+			</section>
+
+		<!-- Two -->
+			<section id="two" class="main style2 left dark fullscreen">
+				<div class="content box style2">
+					<header>
+						<h2>Quem eu sou</h2>
+					</header>
+					<p>Desenvolvo para a WEB há mais de 12 anos. Estudo e sempre me mantenho atualizado com relação à internet, 
+					PHP, padrões WEB e padrões de projeto. Inovar é uma marca que carrego comigo desde
+					o princípio: <i>&quot;Não precisamos reinventar a roda, mas podemos fazê-la girar mais rápido!&quot;</i></p>
+				</div>
+				<a href="#work" class="button style2 down anchored">Próximo</a>
+			</section>
+
+		<!-- Work -->
+			<section id="work" class="main style3 primary">
+				<div class="content container">
+					<header>
+						<h2>Meu trabalho</h2>
+						<p>Lorem ipsum dolor sit amet et sapien sed elementum egestas dolore condimentum.
+						Fusce blandit ultrices sapien, in accumsan orci rhoncus eu. Sed sodales venenatis
+						arcu, id varius justo euismod in. Curabitur egestas consectetur magna vitae.</p>
+					</header>
+
+					<!-- Lightbox Gallery  -->
+						<div class="container 75% gallery">
+							<div class="row 0% images">
+								<div class="6u"><a href="images/fulls/01.jpg" class="image fit from-left"><img src="images/thumbs/01.jpg" title="The Anonymous Red" alt="" /></a></div>
+								<div class="6u"><a href="images/fulls/02.jpg" class="image fit from-right"><img src="images/thumbs/02.jpg" title="Airchitecture II" alt="" /></a></div>
+							</div>
+							<div class="row 0% images">
+								<div class="6u"><a href="images/fulls/03.jpg" class="image fit from-left"><img src="images/thumbs/03.jpg" title="Air Lounge" alt="" /></a></div>
+								<div class="6u"><a href="images/fulls/04.jpg" class="image fit from-right"><img src="images/thumbs/04.jpg" title="Carry on" alt="" /></a></div>
+							</div>
+							<div class="row 0% images">
+								<div class="6u"><a href="images/fulls/05.jpg" class="image fit from-left"><img src="images/thumbs/05.jpg" title="The sparkling shell" alt="" /></a></div>
+								<div class="6u"><a href="images/fulls/06.jpg" class="image fit from-right"><img src="images/thumbs/06.jpg" title="Bent IX" alt="" /></a></div>
+							</div>
+						</div>
+
+				</div>
+			</section>
+
+		<!-- Contact -->
+			<section id="contact" class="main style3 secondary">
+				<div class="content container">
+					<header>
+						<h2>Diga olá.</h2>
+						<p>Responderei o mais brevemente possível seu contato.</p>
+					</header>
+					<div class="box container 75%">
+
+					<!-- Contact Form -->
+							<form method="post" id="frmContato">
+								<div class="row 50%">
+									<div class="6u"><input id="name" type="text" name="name" placeholder="Nome" required /></div>
+									<div class="6u"><input id="email" type="email" name="email" placeholder="Email" required /></div>
+								</div>
+								<div class="row 50%">
+									<div class="12u"><textarea id="message" name="message" placeholder="Mensagem" rows="6" required></textarea></div>
+								</div>
+								<div class="row">
+									<div class="12u">
+										<ul class="actions">
+											<li><input type="submit" id="btnEnviarForm" value="Enviar Mensagem" /></li>
+											<li id="msgRetorno" class=""></li>
+										</ul>
+									</div>
+								</div>
+							</form>
+
+					</div>
+				</div>
+			</section>
+
+		<!-- Footer -->
+			<footer id="footer">
+
+				<!-- Icons -->
+					<ul class="actions">
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li>
+					</ul>
+
+				<!-- Menu -->
+					<ul class="menu">
+						<li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+					</ul>
+
+			</footer>
+
+	</body>
+</html>
